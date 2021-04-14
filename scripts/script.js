9e5c6676f6f964f18365d67c43195da8192383dc
phcatanduba <phcatanduba@gmail.com>
2021-04-13 21:48:37 -0300

    only one option available on menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,49 @@
 const buttonMenu = document.querySelector(".button-menu");
 const menu = document.querySelector(".menu");
 const users = document.querySelectorAll("li");
-const visibility = document.querySelectorAll(".lock")
+const visibilitys = document.querySelectorAll(".lock")
+const back = document.querySelector(".back");
+let userOption = {firstTime: true, lastOption: undefined};
+let visibilityOption = {firstTime: true, lastOption: undefined};
 
 buttonMenu.addEventListener("click", function() {
     show(menu);
 });
 
+back.addEventListener("click", function(){
+    dontShow(menu);
+    user.firstTime = true;
+    visibility.firstTime = true;
+})
+
 users.forEach(user => {
     let userCheck = user.children[1];
     user.addEventListener("click", function () {
-        if(userCheck.classList.contains("on")) {
-            dontShow(userCheck);
-        } else {
-            show(userCheck);
-        };
+        checkOption(userOption, userCheck);
     });
 });
 
-visibility.forEach(option => {
+visibilitys.forEach(option => {
     let optionCheck = option.children[1];
+
     option.addEventListener("click", function () {
-        if(optionCheck.classList.contains("on")) {
-            dontShow(optionCheck);
-        } else {
-            show(optionCheck);
-        };
+        checkOption(visibilityOption, optionCheck);
     });
 });
+
+function checkOption(optionType, option) {
+    if(optionType.firstTime === true) {
+        show(option);
+        optionType.lastOption = option;
+        optionType.firstTime = false;
+    } else if(option.classList.contains("on")) {
+        return;
+    } else {
+        show(option);
+        dontShow(optionType.lastOption);
+        optionType.lastOption = option;
+    };
+}
 
 function show(element) {
     element.classList.add("on");
