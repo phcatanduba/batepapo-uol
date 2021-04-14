0e07ddd48a9dc87adf1d3391b90f7dc355c08b71
phcatanduba <phcatanduba@gmail.com>
2021-04-13 23:21:06 -0300

    send messages added

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 const buttonMenu = document.querySelector(".button-menu");
+const buttonSendMessage = document.querySelector(".box ion-icon");
 const menu = document.querySelector(".menu");
 const users = document.querySelectorAll("li");
-const visibilitys = document.querySelectorAll(".lock")
+const visibilitys = document.querySelectorAll(".lock");
 const back = document.querySelector(".back");
+let warning = document.querySelector(".warning");
 let userOption = {firstTime: true, lastOption: undefined};
 let visibilityOption = {firstTime: true, lastOption: undefined};
 
@@ -29,6 +31,37 @@ visibilitys.forEach(option => {
     });
 });
 
+buttonSendMessage.addEventListener("click", sendMessage);
+
+function sendMessage() {
+    let messages = document.querySelector("main ul");
+    let message = document.querySelector("input");
+    let messageView = 'public';
+    const userName = userOption.lastOption.parentNode.querySelector("span").innerHTML;
+    const isPublic = visibilityOption.lastOption.parentNode.classList.contains("public-visibility");
+    if(!isPublic) {
+        messageView = 'private';
+    }
+    if(message.value !== undefined) {
+        messages.innerHTML +=  `<li class="${messageView}">
+                                    <span class="time">(09:22:28)</span>  
+                                    <span class="name">João</span> 
+                                    para 
+                                    <span class="name">${userName}:</span>  ${message.value}
+                                </li>`;
+    }
+}
+
+function whoWillReceiveTheMessage() {
+    const isPublic = visibilityOption.lastOption.parentNode.classList.contains("public-visibility");
+    const userName = userOption.lastOption.parentNode.querySelector("span").innerHTML;
+    if(isPublic === true) {
+        warning.innerHTML = `Enviando para ${userName} (publicamente)`;
+    }   else {
+        warning.innerHTML = `Enviando para ${userName} (privadamente)`;
+    }
+}
+
 function checkOption(optionType, option) {
     if(optionType.firstTime === true) {
         show(option);
@@ -41,7 +74,9 @@ function checkOption(optionType, option) {
         dontShow(optionType.lastOption);
         optionType.lastOption = option;
     };
-
+    if(!userOption.firstTime && !visibilityOption.firstTime) {
+        whoWillReceiveTheMessage();
+    }
 }
 
 function show(element) {
@@ -51,3 +86,4 @@ function show(element) {
 function dontShow(element) {
     element.classList.remove("on");
 }
+
